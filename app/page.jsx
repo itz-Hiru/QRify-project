14ecf12dec6ce9e1720fc9e13137ddcf322bba3e
itz-Hiru <hirumithagithub@gmail.com>
2025-01-03 09:17:20 +0530

    Created home section tag line

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,14 @@
-"use client"
+"use client";
 
 export default function Home() {
   return (
-    <div>
-
-    </div>
+    <section>
+      <div className="container mx-auto flex flex-col xl:flex-row justify-center xl:justify-start items-center">
+        <div className="pt-5">
+          <h1 className="text-6xl font-bold text-center xl:text-start">QRify</h1>
+          <p className="text-2xl font-semibold text-accent pt-5">Scan, Create Connect</p>
+        </div>
+      </div>
+    </section>
   );
 }
