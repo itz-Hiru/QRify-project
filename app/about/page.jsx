6e10fead8dc946fb61f1c75015e9f259bd0e55d9
itz-Hiru <hirumithagithub@gmail.com>
2025-01-06 19:02:24 +0530

    Added animation to about section

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -4,6 +4,23 @@ import { TextGenerateEffect } from "@/components/ui/text-generate-effect";
 
 const header = `Welcome to QRify`;
 
+const fadeInVariant = (direction) => ({
+  hidden: {
+    opacity: 0,
+    x: direction === "left" ? -100 : 100,
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      type: "spring",
+      stiffness: 50,
+      damping: 50,
+      duration: 2,
+    },
+  },
+});
+
 const page = () => {
   return (
     <motion.section
@@ -12,21 +29,37 @@ const page = () => {
         opacity: 1,
         transition: {
           delay: 0.8,
-          duration: 2,
+          duration: 1.4,
           ease: "easeIn",
         },
       }}
       className="h-full px-4 xl:px-0 mb-6"
     >
       <div className="container">
-        <TextGenerateEffect words={header} className="xl:text-start text-center"/>
-        <p className="xl:pt-8 pt-4 xl:text-[20px] text-[18px] text-white/70 xl:text-start text-justify">
-          We’re here to make sharing, connecting, and accessing information
-          simpler, faster, and smarter. Our platform combines cutting-edge
-          technology with an intuitive design to empower you to create and scan
-          QR codes effortlessly.
-        </p>
-        <div className="mt-5 xl:mt-10">
+        <motion.div
+          variants={fadeInVariant("left")}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+        >
+          <TextGenerateEffect
+            words={header}
+            className="xl:text-start text-center"
+          />
+          <p className="xl:pt-8 pt-4 xl:text-[20px] text-[18px] text-white/70 xl:text-start text-justify">
+            We’re here to make sharing, connecting, and accessing information
+            simpler, faster, and smarter. Our platform combines cutting-edge
+            technology with an intuitive design to empower you to create and
+            scan QR codes effortlessly.
+          </p>
+        </motion.div>
+        <motion.div
+          variants={fadeInVariant("right")}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          className="mt-5 xl:mt-10"
+        >
           <h2 className="xl:text-[36px] text-[24px] text-white/85 font-medium">
             What we offer
           </h2>
@@ -47,8 +80,14 @@ const page = () => {
               styles.
             </li>
           </ul>
-        </div>
-        <div className="mt-5 xl:mt-10">
+        </motion.div>
+        <motion.div
+          variants={fadeInVariant("left")}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          className="mt-5 xl:mt-10"
+        >
           <h2 className="xl:text-[36px] text-[24px] text-white/85 font-medium">
             Our vision
           </h2>
@@ -57,8 +96,14 @@ const page = () => {
             By simplifying QR code technology, we aim to help you share
             information creatively and conveniently.
           </p>
-        </div>
-        <div className="mt-5 xl:mt-10">
+        </motion.div>
+        <motion.div
+          variants={fadeInVariant("right")}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          className="mt-5 xl:mt-10"
+        >
           <h2 className="xl:text-[36px] text-[24px] text-white/85 font-medium">
             Why choose us
           </h2>
@@ -68,7 +113,7 @@ const page = () => {
             <li className="mb-3">Completely free to use</li>
             <li>Built for everyone—developers, businesses, and individuals</li>
           </ul>
-        </div>
+        </motion.div>
       </div>
     </motion.section>
   );
